53aeadaf95cca7d65d65338dfdd03166802941df
Rugwiroparfait <rugwiroparfit003@gmail.com>
2024-10-14 13:16:28 -0700

    3 create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 // routes/index.js
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
 
--- server.js
+++ server.js
@@ -5,8 +5,12 @@ import routes from './routes/index';
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Move this line above your route definitions
+app.use(express.json()); 
+
 app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
