51d11a67c3d3b2af0a1c3e846ec42a29838ff8a0
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-10-16 23:37:58 +0200

    importing AuthController

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-
+import AuthController from '../controllers/AuthController';
 const router = Router();
 
 router.get('/status', AppController.getStatus);
